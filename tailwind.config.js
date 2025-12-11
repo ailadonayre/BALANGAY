189405cdd04d69a82f67639da7c027d650f298c2
Aila Roshiele Donayre <23-02175@g.batstate-u.edu.ph>
2025-12-11 17:11:47 +0800

    Added tailwing config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cream: '#F8F4EE',
+        dark: '#252525',
+      },
+      fontFamily: {
+        futura: ['Futura', 'system-ui', '-apple-system', 'sans-serif'],
+        elinga: ['Elinga', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
